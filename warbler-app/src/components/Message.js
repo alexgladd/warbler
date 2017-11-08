542f788ae8528ecf1e2540420b8ae51859f3d250
Alex Gladd <alex@alexgladd.com>
2017-11-07 19:58:22 -0500

    Use moment to display message dates
    Add border between messages

--- warbler-app/src/components/Message.js
+++ warbler-app/src/components/Message.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import './Message.css';
 
 const Message = ({ message }) => {
   const { author, text, createdAt } = message;
+  const msgDate = moment(createdAt).format('LLL');
+  const dateFromNow = moment(createdAt).fromNow();
 
   return (
     <div className="Message">
       <img className="AuthorImg" src={author.profileImgUrl} alt="Message author avatar" />
       <div className="Author">{author.username}</div>
       <div className="Text">{text}</div>
-      <div className="Info">{createdAt}</div>
+      <div className="Info" title={msgDate}>{dateFromNow}</div>
     </div>
   )
 }
